8539b76e6a6c6009c1984cd031abdc179e6038a8
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-08-24 17:49:18 +0200

    added-second-footer-section

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     <TheNumbersSection/>
     <TheFeedbackSection/>
     <TheFirstFooterSection/>
+    <TheSecondfooterSection/>
   </div>
 </template>
 
@@ -21,9 +22,19 @@ import TheServicesSection from './components/TheServicesSection.vue';
 import TheNumbersSection from './components/TheNumbersSection.vue';
 import TheFeedbackSection from './components/TheFeedbackSection.vue';
 import TheFirstFooterSection from './components/TheFirstFooterSection.vue';
+import TheSecondfooterSection from './components/TheSecondfooterSection.vue';
+
 export default {
   name: 'App',
-  components: { MainHeader, TheJumbotron, TheClientSection, TheFinancialSection, TheServicesSection, TheNumbersSection, TheFeedbackSection, TheFirstFooterSection }
+  components: { MainHeader,
+                TheJumbotron, 
+                TheClientSection, 
+                TheFinancialSection, 
+                TheServicesSection, 
+                TheNumbersSection, 
+                TheFeedbackSection, 
+                TheFirstFooterSection, 
+                TheSecondfooterSection }
 }
 </script>
 
---
+++ src/components/TheSecondfooterSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <section id="second-footer-container">
+   <ul>
+    <li>
+       <img src="../assets/img/finance_logo_footer.png" alt="finance logo">
+    </li>
+   </ul>
+    <div class="recent-posts">
+      <ul>
+        <li><a href="#"><strong>RECENT POSTS</strong></a></li>
+        <li><a href="#">When is it the right time to sell your company?</a></li>
+        <li><a href="#">Personal debt or company debt we explore your option</a></li>
+        <li><a href="#">An interview with a so-called business angel</a></li>
+      </ul>
+    </div>
+    <div class="services">
+      <ul>
+        <li><a href="#"><strong>SERVICES</strong></a></li>
+        <li><a href="#">SMALL BUSINESS LOAN SERVICES</a></li>
+        <li><a href="#">CREDIT RATING ADVICE SERVICES</a></li>
+        <li><a href="#">CROWD FOUNDING SERVICES</a></li>
+      </ul>
+    </div>
+    <div class="latest">
+      <ul>
+        <li><a href="#">LATEST</a></li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default{
+    name:'TheSecondfooterSection'
+  }
+</script>
+
+<style lang="scss">
+  #second-footer-container{
+    background-color: black;
+    display: flex;
+    justify-content: flex-start;
+    li{
+      list-style-type: none;
+      padding: 10px;
+      a{
+        text-decoration: none;
+        color: white;
+      }
+    }
+  }
+</style>
